626dadf13721930699a362b783605712c160ff7c
Felipe Kuhne <fkuhne@gmail.com>
2016-01-20 15:07:50 -0200

    Little adjustment.

--- facedetect/facedetect.cpp
+++ facedetect/facedetect.cpp
@@ -210,14 +210,14 @@ void detectAndDraw( Mat& img, CascadeClassifier& cascade,
         int radius;
 
         double aspect_ratio = (double)r.width/r.height;
-        /*if( 0.75 < aspect_ratio && aspect_ratio < 1.3 )
+        if( 0.75 < aspect_ratio && aspect_ratio < 1.3 )
         {
             center.x = cvRound((r.x + r.width*0.5)*scale);
             center.y = cvRound((r.y + r.height*0.5)*scale);
             radius = cvRound((r.width + r.height)*0.25*scale);
             circle( img, center, radius, color, 3, 8, 0 );
         }
-        else*/
+        else
             rectangle( img, cvPoint(cvRound(r.x*scale), cvRound(r.y*scale)),
                        cvPoint(cvRound((r.x + r.width-1)*scale), cvRound((r.y + r.height-1)*scale)),
                        color, 3, 8, 0);
